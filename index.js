0b3def4954c251d474af856b67e5fc38beb43bbe
Catherine Noel <cnoel@draftkings.com>
2018-08-08 14:50:05 -0400

    .

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ const port = process.env.PORT || 5000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.send({
-    "type": "url_verification",
-	 "token": "OYK6hu327ChWOP3Wlp9IQiYA",
-	 "challenge": "VD5HjwaymQAvwaKG6QvnMyzpIqqE9tIqeRvWYQYLi5JtLA5TYVlI"
-  })
+  res.write('Hello World!\n');
   res.end();
 });
 
