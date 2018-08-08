246f273241fc60145ffc76f46c6070f7f4a9878e
tninesling <tninesling@draftkings.com>
2018-08-08 13:46:31 -0400

    Updated server initialization

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!\n');
+  res.setHeader('Content-Type', 'text/html');
+  res.write('Hello World!\n');
+  res.end();
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
