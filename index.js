ba3a8ed3c1d2c3221244d880220fa0af15b37e80
Catherine Noel <cnoel@draftkings.com>
2018-08-08 16:18:49 -0400

    web client api added

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
-const { RTMClient } = require('@slack/client');
+const { RTMClient,WebClient } = require('@slack/client');
 
-// An access token (from your Slack app or custom integration - usually xoxb)
-const token = process.env.SLACK_TOKEN;
+const token = 'xoxb-414091595655-413066763106-YD3P4JJsp4xLa5RFoKPwlBrR';
+const web = new WebClient(token);
 
 // The client is initialized and then started to get an active connection to the platform
 const rtm = new RTMClient(token);
 rtm.start();
 
-// This argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
-// See the "Combining with the WebClient" topic below for an example of how to get this ID
-const conversationId = 'C1232456';
-
-// The RTM client can send simple string messages
-rtm.sendMessage('Hello there', conversationId)
+web.channels.list()
   .then((res) => {
-    // `res` contains information about the posted message
-    console.log('Message sent: ', res.ts);
-  })
-  .catch(console.error);
+    // Take any channel for which the bot is a member
+    const channel = res.channels.find(c => c.is_member);
+
+    if (channel) {
+      // We now have a channel ID to post a message in!
+      // use the `sendMessage()` method to send a simple string to a channel using the channel ID
+      rtm.sendMessage('See you at 4:30 in Cannons - Bring Coffee', channel.id)
+        // Returns a promise that resolves when the message is sent
+        .then((msg) => console.log(`Message sent to channel ${channel.name} with ts:${msg.ts}`))
+        .catch(console.error);
+    } else {
+      console.log('This bot does not belong to any channel, invite it to at least one and try again');
+    }
+  });
+
+  rtm.on('message', (event) => {
+    // For structure of `event`, see https://api.slack.com/events/message
+  
+    // Skip messages that are from a bot or my own user ID
+    if ( (message.subtype && message.subtype === 'bot_message') ||
+         (!message.subtype && message.user === rtm.activeUserId) ) {
+      return;
+    }
+  
+    // Log the message
+    console.log(`(channel:${message.channel}) ${message.user} says: ${message.text}`);
+  });
